a830d78b26a1d1aeb52e15c3b1135b39e1e89204
Iftekhar Ahmed Arnab <arnab.xero@gmail.com>
2023-10-08 12:10:30 +0600

    no chache

--- app/api/getData/route.js
+++ app/api/getData/route.js
@@ -3,16 +3,26 @@ import { NextResponse } from 'next/server'
 import RATModel from '@/models/ratmodel'
 
 export const GET = async (req, res) => {
-
     try {
-
         await connectToDB();
 
         const allRatModels = await RATModel.find().sort({ updatedAt: -1 });
 
-        return new Response(JSON.stringify({ message: "API GET SUccess", allRatModels }), { status: 200 });
+        // Create the response with Cache-Control header
+        const response = new Response(JSON.stringify({ message: "API GET Success", allRatModels }), { status: 200 });
+
+        // Add Cache-Control: no-cache header
+        response.headers.set('Cache-Control', 'no-cache');
+
+        return response;
 
     } catch (err) {
-        return new Response(JSON.stringify({ message: "API GET Fail" }), { status: 200 });
+        // Create the error response with Cache-Control header
+        const errorResponse = new Response(JSON.stringify({ message: "API GET Fail" }), { status: 500 });
+
+        // Add Cache-Control: no-cache header
+        errorResponse.headers.set('Cache-Control', 'no-cache');
+
+        return errorResponse;
     }
 }
